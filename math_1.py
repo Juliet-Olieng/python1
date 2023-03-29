001ebd2b259aa7e4fd310d2f02dcf7cfb937aa2a
joycewanjiku <joycemumbiiwanjiku@gmail.com>
2023-03-29 20:01:01 +0300

    assign

---
+++ math_1.py
@@ -0,0 +1,18 @@
+def add(a,b):
+    answer = a+b
+    return answer
+def subtraction(a,b):
+    answer =a-b
+    return answer
+def divide(a,b):
+    answer= a/b
+    return answer
+def multiply(a,b):
+    answer =a*b
+    return answer
+def remainder(a,b):
+    answer= a%b
+    return answer
+
+
+
